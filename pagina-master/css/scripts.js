5b019e0118594778e602f2c52be695dfcea38609
camilo <camilosoler22@hotmail.com>
2019-05-17 07:55:18 -0500

    camilo soler escritura

---
+++ pagina-master/css/scripts.js
@@ -0,0 +1,19 @@
+// var persona = {
+//     nombre: ['Kurt', 'Rodriguez'],
+//     edad: 22,
+//     genero: 'masculino',
+//     intereses: ['música', 'BMX'],
+//     bio: function () {
+//       alert(this.nombre[0] + '' + this.nombre[1] + ' tiene ' + this.edad + ' años. Le gusta ' + this.intereses[0] + ' y ' + this.intereses[1] + '.');
+//     },
+//     saludo: function() {
+//       alert('Hola, Soy '+ this.nombre[0] + '. ');
+//     }
+//   };
+
+// persona.nombre
+// persona.nombre[0]
+// persona.edad
+// persona.intereses[1]
+// persona.bio()
+// persona.saludo()
---
+++ pagina-master/js/java.js
@@ -0,0 +1,12 @@
+document.querySelector('subtitulo').addEventListener('click', ()=> {
+    document.body.style.background = 'white';
+})
+document.querySelector('h1').addEventListener('click', ()=> {
+    document.body.style.background = 'green';
+})  
+document.querySelector('h1').addEventListener('click', ()=> {
+    document.body.style.background = 'green';
+})
+document.querySelector('h1').addEventListener('click', ()=> {
+    document.body.style.background = 'green';
+})
